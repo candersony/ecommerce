dfed0687b9ebe3281e7c0c320cac413a40139046
Craig Anderson <candersony@users.noreply.github.com>
2021-05-24 20:43:33 +0100

    useProducts

---
+++ src/product/model/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  readonly id: number;
+  readonly title: string;
+  readonly description: string;
+  readonly category: string;
+  readonly image: string;
+  readonly price: number;
+}
---
+++ src/product/use-products.ts
@@ -0,0 +1,20 @@
+import { ref, onMounted, Ref } from "vue";
+import { Product } from "./model/product";
+
+export function useProducts(): {
+  products: Ref<Product[]>;
+  getProducts: () => Promise<void>;
+} {
+  const products = ref(new Array<Product>());
+  const getProducts = async () =>
+    (products.value = await fetch("https://fakestoreapi.com/products").then(
+      (res) => res.json()
+    ));
+
+  onMounted(getProducts);
+
+  return {
+    products,
+    getProducts,
+  };
+}
