5668f824a8f275e820690042483379cd6b7b2480
Craig Anderson <candersony@users.noreply.github.com>
2021-05-24 20:47:45 +0100

    carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <el-carousel indicator-position="none">
+      <el-carousel-item v-for="product in featured" :key="product.id">
+        <ProductTile v-bind="product" />
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Product } from "@/product/model/product";
+import ProductTile from "@/product/components/ProductTile.vue";
+import { useProducts } from "@/product/use-products";
+
 export default {
   name: "Home",
-  components: {},
+  components: { ProductTile },
+  setup: useProducts,
+  computed: {
+    featured(): Product[] {
+      return this.products.slice(0, 5);
+    },
+  },
 };
 </script>
