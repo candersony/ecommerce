de82bf5ab1759a112907ce409e952e44d32cdf7f
Craig Anderson <candersony@users.noreply.github.com>
2021-05-24 20:43:06 +0100

    Footer with links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,27 @@
-<template>footer</template>
+<template>
+  <el-menu mode="horizontal">
+    <el-menu-item index="1">
+      <router-link to="/">Home</router-link>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <router-link to="about">About us</router-link>
+    </el-menu-item>
+  </el-menu>
+</template>
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "Footer",
+  data() {
+    return {
+      activeIndex: "1",
+    };
+  },
 });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-menu {
+  width: 100%;
+  border-top: 1px solid grey;
+}
+</style>
